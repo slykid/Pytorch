b652623e4babd4dffdbd40ef227f0939433adcac
slykid <slykid@naver.com>
2021-04-29 22:46:55 +0900

    Pytorch Basic: 3. Prepare training

--- Basic/3_prepare_training/2_Use_custom_dataset.py
+++ Basic/3_prepare_training/2_Use_custom_dataset.py
@@ -15,17 +15,26 @@ for epoch in range(20):
 
 # 예시 2.
 import os
+import numpy as np
 from matplotlib import pyplot as plt
 
 from torch.utils.data import DataLoader
+from torchvision import transforms
+
 import DogCatDataset as dogcat
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 path = "data\\dogs_vs_cats"
 
-datasets = dogcat.DogCatDataset(root_path=path)
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# 데이터 셋, 데이터 로더 생성
+datasets = dogcat.DogCatDataset(root_path=path, transform=transform)
 dataloader = DataLoader(datasets, batch_size=10, shuffle=True)
 
 for image, label in dataloader:
-    plt.imshow(image)
-    print(label)
+    pass
--- Basic/3_prepare_training/DogCatDataset.py
+++ Basic/3_prepare_training/DogCatDataset.py
@@ -6,37 +6,48 @@ from glob import glob
 from torch.utils.data import Dataset
 
 class DogCatDataset(Dataset):
-    def __init__(self, root_path, size=(224, 224)):
+    def __init__(self, root_path, transform=None):
         self.root_path = root_path
-        self.size = size
-
-        self.files = glob(os.path.join(self.root_path, "*.jpg"))
-        self.size = len(self.files)
+        self.transform = transform
 
         for t in ['train', 'valid']:
             for folder in ['dog', 'cat']:
+                # 폴더가 없는 경우
                 if os.path.isdir(os.path.join(self.root_path, t, folder)) is False:
                     os.makedirs(os.path.join(self.root_path, t, folder))
+                    self.files = glob(os.path.join(self.root_path, "*.jpg"))
+                    self.size = len(self.files)
+                    print(self.size)
 
-        if self.size > 0:
-            shuffle = np.random.permutation(self.size)
-            # valid
-            for i in shuffle[:2000]:
-                folder = self.files[i].split('\\')[-1].split('.')[0]
-                image = self.files[i].split('\\')[-1]
-                os.rename(self.files[i], os.path.join(self.root_path, 'valid', folder, image))
+                    if self.size > 0:
+                        shuffle = np.random.permutation(self.size)
 
-            # train
-            for i in shuffle[2000:]:
-                folder = self.files[i].split('\\')[-1].split('.')[0]
-                image = self.files[i].split('\\')[-1]
-                os.rename(self.files[i], os.path.join(self.root_path, 'train', folder, image))
+                        # valid
+                        for i in shuffle[:2000]:
+                            folder = self.files[i].split('\\')[-1].split('.')[0]
+                            image = self.files[i].split('\\')[-1]
+                            os.rename(self.files[i], os.path.join(self.root_path, 'valid', folder, image))
 
+                        # train
+                        for i in shuffle[2000:]:
+                            folder = self.files[i].split('\\')[-1].split('.')[0]
+                            image = self.files[i].split('\\')[-1]
+                            os.rename(self.files[i], os.path.join(self.root_path, 'train', folder, image))
+
+                # 폴더가 생성된 경우
+                else:
+                    self.files = glob(os.path.join(self.root_path, t, "*", "*.jpg"))
+                    self.size = len(self.files)
+                    print(self.size)
 
     def __len__(self):
         return self.size
 
     def __getitem__(self, index):
-        img = np.asarray(Image.open(self.files[index]).resize(self.size))
-        label = self.files[index].split("\\")[-2]
+        img = Image.open(self.files[index])
+        label = self.files[index].split('\\')[-1].split('.')[0]
+
+        if self.transform:
+            img = self.transform(img)
+
         return img, label
